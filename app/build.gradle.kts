ecedb1a15b8bed141bcbd7f546405e95c0704ce7
xihale <xihale.top@qq.com>
2026-08-24 18:22:36 +0800

    chore: 版本号升至 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.easytier.android.native"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
